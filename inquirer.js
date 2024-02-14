38f94b07719a21e8c3feb3bdd630100ad17b9a8e
Boubacar Diallo <bd015720@gmail.com>
2024-02-14 12:01:28 +0000

    first commit

---
+++ inquirer.js
@@ -0,0 +1,37 @@
+import inquirer from 'inquirer'
+
+const start = async () => {
+    let {result} = await inquirer.prompt({
+        name: "result",
+        type: "confirm",
+        message: "Do you want to start Escape Room?"
+    })
+    return result
+}
+
+const character = async () => {
+    let {result} = await inquirer.prompt({
+        name: "result",
+        type: "input",
+        message: "Name your character"
+    })
+    return result
+}
+
+const difficulty = async () =>{
+    let {result} = await inquirer.prompt({
+        name:"result",
+        type:"list",
+        message:"Select difficulty",
+        choices:["Easy","Medium","Hard"]
+    })
+    return result
+}
+
+
+const display = async () => {
+    let started = await start()
+    console.log(started)
+}
+
+display()
